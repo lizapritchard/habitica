bc3e53b4cb4fa8bf547b7fafe5f403e8aa2de3c4
Lizapritchardg <lizap@umich.edu>
2021-05-31 16:35:10 -0400

    fix watcher to update challenges on group change

--- website/client/src/components/challenges/groupChallenges.vue
+++ website/client/src/components/challenges/groupChallenges.vue
@@ -108,8 +108,10 @@ export default {
     },
   },
   watch: {
-    async groupId () {
-      this.loadChallenges();
+    'group._id': {
+      async groupId () {
+        this.loadChallenges();
+      },
     },
   },
   mounted () {
